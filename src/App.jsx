efae016f718377588d6a8ca95a070c2c01eb9fcf
Wael Aloush <waelee2018@gmail.com>
2025-10-05 20:05:14 +0300

    f

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import * as React from "react";
+
+import "./App.css";
+import theme from "./Theme";
+
+// MUI COMPONENTS
+import Typography from "@mui/material/Typography";
+import { ThemeProvider } from "@mui/material/styles";
+import Container from "@mui/material/Container";
+
+// == MUI COMPONENTS==
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ThemeProvider theme={theme}>
+        <Container maxWidth="sm" className="bg-blue-900 rounded-md m-0 p-2!">
+          {/* card */}
+          <div>
+            {/* content */}
+            <div>
+              {/* city && time */}
+              <div
+                className="flex justify-start items-end gap-3 my-2 mx-3"
+                dir="rtl"
+              >
+                <Typography variant="h2" color="white">
+                  الرياض
+                </Typography>
+                <Typography variant="h5" color="white">
+                  الاربعاء 10/10-2025 04:45
+                </Typography>
+              </div>
+              {/*== city && time ==*/}
+              <hr className="w-full" />
+              {/* degree && discription */}
+              <div></div>
+              {/*== degree && discription ==*/}
+            </div>
+            {/*== content ==*/}
+          </div>
+          {/*== card ==*/}
+        </Container>
+      </ThemeProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Theme.jsx
@@ -0,0 +1,9 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "IBM, sans-serif",
+  },
+});
+
+export default theme;
